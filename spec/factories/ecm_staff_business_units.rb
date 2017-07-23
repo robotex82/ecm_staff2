29f35892a8c27e085d90616eeb2f0a48c0b68da4
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-07-23 21:21:20 +0200

    Add missing attribute markup_language to business unit factory.

--- spec/factories/ecm_staff_business_units.rb
+++ spec/factories/ecm_staff_business_units.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
   factory :ecm_staff_business_unit, class: Ecm::Staff::BusinessUnit do
     association :organisation, factory: :ecm_staff_organisation
     sequence(:name) { |i| "Business Unit ##{i}" }
+    markup_language 'textile'
   end
 end
